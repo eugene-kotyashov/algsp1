e18d20c8ce328b00bee637be70673b05630c6cbd
unknown <eugene.kotyashov@gmail.com>
2017-11-05 17:31:31 +0300

    fixed backwash by using another WeightedUF object

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -5,8 +5,9 @@
  * 
  *  Description:  Class for percolation simulation of a NxN
  *         grid using WeightedQuickUnion implementation for sites
- *         interconnection last modified 28.12.2013
+ *         interconnection
  * @author eugene.kotyashov@gmail.com
+ * 
 ******************************************************************************/
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
@@ -24,19 +25,23 @@ private int openSiteCount;
 // value of 1 means site is full and 0 - open
 private byte[] grid;
 
-private WeightedQuickUnionUF uf;
+private WeightedQuickUnionUF uf, ufFillCheck;
 
     
 public Percolation(int N) // create N-by-N grid, with all sites blocked
 {
+    if (N < 1) 
+        throw new java.lang.IllegalArgumentException();
     sizeN = N;
     openSiteCount = 0;
     gridSize = sizeN * sizeN;
     grid = new byte[gridSize];
 // int uf instance with grid size plus 2 for virtual sites
     uf = new WeightedQuickUnionUF(gridSize + 2);
+    ufFillCheck = new WeightedQuickUnionUF(gridSize + 1);
     for (int i = 0; i < sizeN; i++) {
         uf.union(0, i + 1);
+        ufFillCheck.union(0, i + 1);
         uf.union(gridSize + 1, gridSize - i);
     }
     for (int i = 0; i < gridSize; i++) {
@@ -73,18 +78,22 @@ public void open(int i, int j) // open site (row i, column j) if it is not
         //check connection with upper site
         if ((i < sizeN) && isOpen(i + 1, j)) {
             uf.union(gridToUFArray(i, j), gridToUFArray(i + 1, j));
+            ufFillCheck.union(gridToUFArray(i, j), gridToUFArray(i + 1, j));
         }
         //check connection with lower cite
         if ((i > 1) && isOpen(i - 1, j)) {
             uf.union(gridToUFArray(i, j), gridToUFArray(i - 1, j));
+            ufFillCheck.union(gridToUFArray(i, j), gridToUFArray(i - 1, j));
         }
         //check connection with left site
         if ((j > 1) && isOpen(i, j - 1)) {
             uf.union(gridToUFArray(i, j), gridToUFArray(i, j - 1));
+            ufFillCheck.union(gridToUFArray(i, j), gridToUFArray(i, j - 1));
         }
         //check connection with right site
         if ((j < sizeN) && isOpen(i, j + 1)) {
             uf.union(gridToUFArray(i, j), gridToUFArray(i, j + 1));
+            ufFillCheck.union(gridToUFArray(i, j), gridToUFArray(i, j + 1));
         }
     }
     
@@ -102,7 +111,7 @@ public boolean isOpen(int i, int j) // is site (row i, column j) open?
 public boolean isFull(int i, int j) // is site (row i, column j) full?
 {    
     if (isOpen(i, j)) {
-        return uf.connected(0, gridToUFArray(i, j));
+        return ufFillCheck.connected(0, gridToUFArray(i, j));
     } else {
         return false;
     }
@@ -114,7 +123,10 @@ public int numberOfOpenSites()       // number of open sites
 }
 public boolean percolates() // does the system percolate?
 {
-    return uf.connected(0, gridSize + 1);
+    if (openSiteCount > 0)
+        return uf.connected(0, gridSize + 1);
+    else
+        return false;
 }
 
 }
